5d2efe05addcc2c3d530c534f69bd0ae3c12457d
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-13 13:22:03 -0300

    Trabalhando com arrays

---
+++ Secao-2/Ex2/script.js
@@ -0,0 +1,9 @@
+var lista = ['Matheus', 'Nicolas', 'Lucas', 150];
+
+lista[0];
+lista.indexOf('Matheus'); // retorna a posição do item da lista
+lista[0] = 'José'; // determina o valor do index 0 da lista
+lista.push('Matheus'); // adiciona o item para a lista
+lista.shift(); // remove o primeiro item da lista
+lista.pop(); // remove o ultimo item da lista
+lista.join('; '); // separa os itens da lista pelo valor definido
