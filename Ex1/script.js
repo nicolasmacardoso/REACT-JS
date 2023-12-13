61f1297f57a07e0cc05f4ea8b74aeec3534e7b31
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-13 12:59:26 -0300

    Entendendo funções pt. 2

--- Ex1/script.js
+++ Ex1/script.js
@@ -22,3 +22,22 @@ function sair(){
 
     alert("Até mais!");
 }
+
+function mediaAluno(nota1, nota2) {
+    var media = (nota1 + nota2) / 2;
+
+    if (media >= 7 && media <= 10) {
+        console.log("Aluno aprovado com a média: " + media);
+    } else if (media < 7 && media >= 5) {
+        console.log("Aluno em recuperação com a média: " + media)
+    } else if (media < 5 && media >= 0) {
+        console.log("Aluno reprovado com a média: " + media)
+    } else {
+        console.log("Notas inválidas! A média não pode ser menor que 0 ou maior que 10.")
+    }
+}
+
+function Aluno(nome, curso){
+    var mensagem = "Seja bem vindo" + nome + " ao curso de " + curso;
+    console.log(mensagem);
+}
