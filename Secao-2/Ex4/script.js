41838f1708b430dcb4ab8fa85ee8dfee8239bacb
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-14 20:29:53 -0300

    Manipulando datas

--- Secao-2/Ex4/script.js
+++ Secao-2/Ex4/script.js
@@ -1,5 +1,26 @@
 var data = new Date();
 
-data.getHours();
-data.getMinutes();
-data.getSeconds();
+var horas = data.getHours();
+var minutos = data.getMinutes();
+var segundos = data.getSeconds();
+
+var datanova = new Date("March 10, 2018");
+
+var milissegundos = Date.parse(datanova);
+
+var data2 = new Date(1520650800000);
+
+var dia = datanova.getDate();
+var mes = datanova.getMonth() + 1;
+var semana = datanova.getDay();
+var ano = datanova.getFullYear();
+
+var dataFormatada = dia + "/" + mes + "/" + ano;
+
+datanova.setDate(dia + 5);
+
+var dias = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+var hoje = dias[semana];
+
+var horario = horas + ":" + minutos + ":" + segundos;
