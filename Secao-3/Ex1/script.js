e51e5fe85bb152079da36bf40da4ace1899965e5
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-19 19:16:38 -0300

    Mexendo com setTimeout

--- Secao-3/Ex1/script.js
+++ Secao-3/Ex1/script.js
@@ -1,10 +1,22 @@
 
-/* function acao(){
+function acao(){
     document.write("Executando... <br>");
 }
 
-setInterval(acao, 1000); */
+/* 
+setInterval(acao, 1000);
+ */
 
+/* 
 var timer = setInterval(() => {
     document.write("Executando...<br>")
-}, 1000);
+}, 1000); 
+*/
+
+/* 
+setTimeout(acao, 3000)
+ */
+
+setTimeout(() => {
+    console.log("Executou o timeout")
+}, 3000);
