c0c8aa89e9583fafa4c458772c3c01357133632e
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-19 19:39:12 -0300

    Variáveis dentro de string

---
+++ Secao-3/Ex4 - Variáveis dentro de string/script.js
@@ -0,0 +1,9 @@
+let nome = "Nícolas";
+let sobrenome = "Machado";
+let idade = 18;
+
+// let mensagem = "Meu nome é " + nome + " " + sobrenome + " e eu tenho " + idade + " anos de idade";
+
+let mensagem = `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos de idade`;
+
+console.log(mensagem);
