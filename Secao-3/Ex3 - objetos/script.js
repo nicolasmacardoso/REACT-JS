9a7df46d13b9534547ce1ba4c81226778d5bd160
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-19 19:33:09 -0300

    Objetos

---
+++ Secao-3/Ex3 - objetos/script.js
@@ -0,0 +1,41 @@
+let pessoa = {
+    nome: 'Matheus',
+    cargo: "Programador fullstack",
+    idade: 25,
+    peso: 70,
+    altura: 1.80,
+}
+
+let carro = {
+    marca: 'Volkswagen',
+    modelo: 'Jetta',
+    ano: 2020,
+    velocidade: 100,
+}
+
+console.log(pessoa)
+console.log(carro)
+
+let usuarios = [
+    {nome: 'Matheus',
+    cargo: "Programador fullstack",
+    idade: 25,
+    peso: 70,
+    altura: 1.80,},
+
+    {nome: 'Pedro',
+    cargo: "Programador fullstack",
+    idade: 18,
+    peso: 65,
+    altura: 1.65,},
+
+    {nome: 'Carlos',
+    cargo: "Programador fullstack",
+    idade: 54,
+    peso: 76,
+    altura: 1.78,}
+];
+
+usuarios[1].nome // 'Pedro'
+
+console.log(usuarios)
