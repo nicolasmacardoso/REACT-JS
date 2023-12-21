e672b850716f792750c1f2b092cd0f925ad9d92a
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-21 16:45:20 -0300

    Includes, startsWith e endsWith

---
+++ Secao-3/Ex11 - Includes, startsWith, endsWith/script.js
@@ -0,0 +1,15 @@
+let nomes = ["Nícolas", "Lucas", "Miguel"];
+
+console.log(nomes.includes("Nícolas"));
+
+if (nomes.includes("Lucas")){
+    console.log("Lucas está na lista");
+} else{
+    console.log("Lucas não está na lista");
+}
+
+let nome = "Nícolas";
+
+console.log(nome.startsWith("Ní"));
+
+console.log(nome.endsWith("as"));
