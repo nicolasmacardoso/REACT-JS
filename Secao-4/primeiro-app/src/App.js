9db4bd597ed16266c7d7a25da0d203e3815e140c
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-22 14:56:21 -0300

    Props e componentes

--- Secao-4/primeiro-app/src/App.js
+++ Secao-4/primeiro-app/src/App.js
@@ -1,9 +1,15 @@
-function App() {
+import Nome from './components/Nome'
+
+function App(){
   return (
     <div>
-        <h1>Bem vindo ao meu projeto</h1>
+      <h1>Componente App</h1>
+      <Nome aluno="Nícolas" idade={30}/>  
+      <br/>
+      <Nome aluno="Felipe" idade={25}/>
     </div>
   );
 }
 
 export default App;
+
