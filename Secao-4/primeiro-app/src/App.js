02ac6beb549d0d3d31bf687e4fdac11881043963
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-22 15:17:48 -0300

    Manipulando formulários

--- Secao-4/primeiro-app/src/App.js
+++ Secao-4/primeiro-app/src/App.js
@@ -1,24 +1,62 @@
 import { useState } from 'react';
 
-import Nome from './components/Nome'
-
 function App(){
-  const [aluno, setAluno] = useState('Nícolas');
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
   const [idade, setIdade] = useState(0);
 
-  function handleChangeName(nome){
-    setAluno(nome);
+  const [user, setUser] = useState('');
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    })
+
+    alert('Usuário registrado com sucesso!')
   }
 
   return (
     <div>
-      <h1>Componente App</h1><br/>
-      <h2>Olá: {aluno}</h2>
-      <button onClick={ () => handleChangeName('Fernando Machado') }>
-        Mudar nome
-      </button>
-{/*       <Nome aluno="Miguel" idade={30}/>  */}    
- </div>
+      <h1>Cadastrando usuario</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome: </label><br/>
+        <input 
+          placeholder='Digite seu nome...'
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          /><br/>
+
+        <label>Email: </label><br/>
+        <input 
+          placeholder='Digite seu email...'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          /><br/>
+
+        <label>Idade: </label><br/>
+        <input 
+          placeholder='Digite sua idade...'
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+          /><br/>
+
+        <button type='submit'>Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <div>
+        <span>Bem vindo: {user.nome}</span> <br/>
+        <span>Idade: {user.idade}</span> <br/>
+        <span>Email: {user.email}</span> <br/>
+      </div>
+
+    </div>
   );
 }
 
