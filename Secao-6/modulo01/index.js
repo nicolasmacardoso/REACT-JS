35d13835c65f158761297f8b717d1a5a31159acc
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-25 12:03:32 -0300

    Entendendo query, route params

--- Secao-6/modulo01/index.js
+++ Secao-6/modulo01/index.js
@@ -2,8 +2,17 @@ const express = require('express')
 
 const server = express();
 
-server.get('/curso', (req, res) => {
-    return res.json({curso: 'Node JS'});
+// Query params = ?nome=NodeJS
+// Route Params = /curso/2
+// Request Body = { nome: 'NodeJS', tipo: 'Backend' }
+
+// localhost:3000/curso
+server.get('/curso/:id', (req, res) => {
+/*     const nome = req.query.nome; */    
+const id = req.params.id;
+
+
+    return res.json({curso: /* `Aprendendo ${nome}` */` Curso ${id}`});
 })
 
 server.listen(3000);
