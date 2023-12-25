dc27d35e4bb2b32f208ddf2d365d9cc1e96fa1e3
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2023-12-25 12:41:36 -0300

    Começando um CRUD

--- Secao-6/modulo01/index.js
+++ Secao-6/modulo01/index.js
@@ -2,18 +2,48 @@ const express = require('express')
 
 const server = express();
 
+server.use(express.json());
 // Query params = ?nome=NodeJS
 // Route Params = /curso/2
 // Request Body = { nome: 'NodeJS', tipo: 'Backend' }
 
+// localhost:3000/cursos
+
 const cursos = ['Node JS', 'JavaScript', 'React Native'];
 
+server.get('/cursos', (req, res) => {
+    return res.json(cursos);
+});
 
-// localhost:3000/curso
-server.get('/curso/:index', (req, res) => {
+server.get('/cursos/:index', (req, res) => {
     const { index } = req.params;
 
     return res.json(cursos[index]);
 })
+
+server.post('/cursos', (req, res) => {
+    const { nome } = req.body;
+
+    cursos.push(nome);
+
+    return res.json(cursos);
+});
+
+server.put('/cursos/:index', (req, res) => { 
+    const { index } = req.params;
+    const { nome } = req.body;
+    
+    cursos[index] = nome;
+    
+    return res.json(cursos);
+});
+
+server.delete('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    
+    cursos.splice(index, 1);
+    
+    return res.json({message: "Curso deletado com sucesso!"});
+});
 
 server.listen(3000);
