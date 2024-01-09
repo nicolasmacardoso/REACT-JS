f97d4bf9329015bec94c75d9e35104561d88f337
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-09 15:15:38 -0300

    Entendendo estrutura

---
+++ Secao-8/estudo/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Aprendendo NextJS</h1>
+      <h2>@nicolasmacardoso</h2>
+    </div>
+  )
+}
---
+++ Secao-8/estudo/pages/sobre.js
@@ -0,0 +1,7 @@
+export default function Sobre(){
+    return(
+        <div>
+            <h1>Página sobre</h1>
+        </div>
+    )
+}
