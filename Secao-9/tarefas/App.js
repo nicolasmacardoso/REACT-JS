85854e9d835bffb85bc33a1c8cb725808089e10b
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-15 22:52:08 -0300

    Aplicando grupo de estilos

--- Secao-9/tarefas/App.js
+++ Secao-9/tarefas/App.js
@@ -1,11 +1,46 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [ nome, setNome ] = useState('Fulano')
+
+  function handleMudarNome() {
+    setNome("Lucas")
+  }
+
   return (
-    <View style={{ flex: 1, backgroundColor: '#121212', paddingTop: 28 }}>
-      <Text style={{ fontSize: 28}}>Nícolas</Text>
-      <Text>Programador</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{nome}</Text>
+
+      <TouchableOpacity style={styles.button} onPress={handleMudarNome}>
+        <Text style={styles.buttonText}>Mudar nome</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+    paddingTop: 100,
+  },
+  title: {
+    fontSize: 32,
+    color: '#121212',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'blue',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 35,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
+
+});
